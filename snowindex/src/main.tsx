e22d5e999d8e33bc61284be953855018b481a82a
jimmyxuexue <1002661758@qq.com>
2022-07-25 00:27:24 +0800

    :tada: 路由&tailwindcss 配置

--- snowindex/src/main.tsx
+++ snowindex/src/main.tsx
@@ -1,10 +1,21 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import { BrowserRouter as Router, Routes, Route, Link } from 'react-router-dom'
 import './index.css'
 
+function Home() {
+	return <div>hello world</div>
+}
+
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		{/* <App /> */}
+		<Router>
+			<Routes>
+				<Route path="/" element={<App />} />
+				<Route path="/home" element={<Home />} />
+			</Routes>
+		</Router>
+	</React.StrictMode>
 )
---
+++ snowindex/tailwind.config.cjs
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+	theme: {
+		fontFamily: {
+			// sans: ['ui-sans-serif', 'system-ui'],
+			sans: ['courier-new', 'courier', 'monospace'],
+		},
+		extend: {},
+	},
+	plugins: [],
+}
