144f7ce075960f67547e7179574278406e395620
jimmyxuexue <1002661758@qq.com>
2022-10-03 00:14:27 +0800

    :zap: varbook指令性能优化

--- src/components/Varbook.tsx
+++ src/components/Varbook.tsx
@@ -1,5 +1,5 @@
 import { memo, useMemo } from 'react'
-import { Space, Table, Tag } from 'antd'
+import { Table } from 'antd'
 import type { ColumnsType } from 'antd/es/table'
 import './cover.css'
 import { TVarBookResult } from 'types/TVarbook'
@@ -30,41 +30,60 @@ export default memo(({ book }: TProps) => {
 	]
 
 	const data: DataType[] = useMemo(() => {
-		console.log('book~~', book)
-		return [
+		const { namedVariables } = book
+		const list = [
 			{
 				key: '1',
 				name: '小驼峰',
-				value: 'Hello',
+				value: '',
 			},
 			{
 				key: '2',
 				name: '大驼峰',
-				value: 'Hello',
+				value: '',
 			},
 			{
 				key: '3',
 				name: '小蛇型',
-				value: 'Hello',
+				value: '',
 			},
 			{
 				key: '4',
 				name: '大蛇型',
-				value: 'Hello',
+				value: '',
 			},
 			{
 				key: '5',
 				name: '脊柱',
-				value: 'Hello',
+				value: '',
 			},
 			{
 				key: '6',
 				name: '注释',
-				value: 'Hello',
+				value: '',
 			},
 		]
-	}, [book])
 
+		for (const key in namedVariables) {
+			// @ts-ignore
+			const value = namedVariables[key]
+			switch (key) {
+				case 'bigHump':
+					list[1].value = value
+				case 'bigSnake':
+					list[3].value = value
+				case 'note':
+					list[5].value = value
+				case 'smallHump':
+					list[0].value = value
+				case 'smallSnake':
+					list[2].value = value
+				case 'spinal':
+					list[4].value = value
+			}
+		}
+		return list
+	}, [book])
 	return (
 		<div className="varbook-dz my-2">
 			<Table
