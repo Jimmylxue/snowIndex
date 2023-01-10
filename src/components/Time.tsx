b688cec051395c3eeadcb86008b8d01452949c09
jimmyxuexue <1002661758@qq.com>
2023-01-10 19:07:26 +0800

    💄 描述文案信息更新

--- src/components/Time.tsx
+++ src/components/Time.tsx
@@ -1,90 +1,90 @@
-import { memo, useMemo } from 'react'
-import { Progress, Statistic } from 'antd'
-import { useTimeInfo } from '@/hooks/useTimeInfo'
+import { memo, useMemo } from 'react';
+import { Progress, Statistic } from 'antd';
+import { useTimeInfo } from '@/hooks/useTimeInfo';
 
-const { Countdown } = Statistic
+const { Countdown } = Statistic;
 
 export default memo(() => {
-	const {
-		weekStart,
-		workProgress,
-		outWork,
-		endWork,
-		nowHour,
-		nowMinutes,
-		nowWeak,
-		weekProgress,
-	} = useTimeInfo({
-		workStartTime: '9:00:00',
-		workEndTime: '18:00:00',
-	})
+  const {
+    weekStart,
+    workProgress,
+    outWork,
+    endWork,
+    nowHour,
+    nowMinutes,
+    nowWeak,
+    weekProgress,
+  } = useTimeInfo({
+    workStartTime: '9:00:00',
+    workEndTime: '18:00:00',
+  });
 
-	const isWorkDay = useMemo(() => {
-		return nowWeak <= 5
-	}, [nowWeak])
+  const isWorkDay = useMemo(() => {
+    return nowWeak <= 5;
+  }, [nowWeak]);
 
-	const showOffWork = useMemo(() => {
-		if (isWorkDay && nowHour < 18) {
-			return (
-				<Countdown
-					valueStyle={{
-						color: '#fff',
-					}}
-					style={{
-						color: '#fff',
-					}}
-					title={<div className=" text-white -mb-2">距离下班：</div>}
-					value={endWork}
-					format="HH:mm:ss:SSS"
-				/>
-			)
-		} else {
-			return <p className="mb-1">下班时间，该学习啦~</p>
-		}
-	}, [nowWeak, nowHour, nowMinutes])
+  const showOffWork = useMemo(() => {
+    if (isWorkDay && nowHour < 18) {
+      return (
+        <Countdown
+          valueStyle={{
+            color: '#fff',
+          }}
+          style={{
+            color: '#fff',
+          }}
+          title={<div className=' text-white -mb-2'>距离下班：</div>}
+          value={endWork}
+          format='HH:mm:ss:SSS'
+        />
+      );
+    } else {
+      return <p className='mb-1'>下班时间，该学习啦~</p>;
+    }
+  }, [nowWeak, nowHour, nowMinutes]);
 
-	return (
-		<div className="my-1" id="timeNode">
-			{isWorkDay ? (
-				<Progress
-					percent={workProgress}
-					status="active"
-					strokeColor={{ '0%': '#108ee9', '100%': '#87d068' }}
-				/>
-			) : (
-				<Progress
-					percent={weekProgress}
-					status="active"
-					strokeColor={{ '0%': '#108ee9', '100%': '#87d068' }}
-				/>
-			)}
+  return (
+    <div className='my-1' id='timeNode'>
+      {isWorkDay ? (
+        <Progress
+          percent={workProgress}
+          status='active'
+          strokeColor={{ '0%': '#108ee9', '100%': '#87d068' }}
+        />
+      ) : (
+        <Progress
+          percent={weekProgress}
+          status='active'
+          strokeColor={{ '0%': '#108ee9', '100%': '#87d068' }}
+        />
+      )}
 
-			{showOffWork}
-			{nowWeak <= 5 ? (
-				<Countdown
-					valueStyle={{
-						color: '#fff',
-					}}
-					style={{
-						color: '#fff',
-					}}
-					title={<div className=" text-white -mb-2">距离周末：</div>}
-					value={outWork}
-					format="D 天 H 时 m 分 s 秒"
-				/>
-			) : (
-				<Countdown
-					valueStyle={{
-						color: '#fff',
-					}}
-					style={{
-						color: '#fff',
-					}}
-					title={<div className=" text-white">周末体验卡剩余时间：</div>}
-					value={weekStart}
-					format="D 天 H 时 m 分 s 秒"
-				/>
-			)}
-		</div>
-	)
-})
+      {showOffWork}
+      {nowWeak <= 5 ? (
+        <Countdown
+          valueStyle={{
+            color: '#fff',
+          }}
+          style={{
+            color: '#fff',
+          }}
+          title={<div className=' text-white -mb-2'>距离周末：</div>}
+          value={outWork}
+          format='D 天 H 时 m 分 s 秒'
+        />
+      ) : (
+        <Countdown
+          valueStyle={{
+            color: '#fff',
+          }}
+          style={{
+            color: '#fff',
+          }}
+          title={<div className=' text-white'>周末体验卡剩余时间：</div>}
+          value={weekStart}
+          format='D 天 H 时 m 分 s 秒'
+        />
+      )}
+    </div>
+  );
+});
--- src/components/Welcome.tsx
+++ src/components/Welcome.tsx
@@ -1,54 +1,56 @@
-import { TWelcomeType } from '@/stores/reducer/welcome'
-import { memo, useEffect } from 'react'
-import { useSelector } from 'react-redux'
-import { EnvironmentOutlined, LoadingOutlined } from '@ant-design/icons'
-import { useLocation } from '@/hooks/useLocation'
+import { TWelcomeType } from '@/stores/reducer/welcome';
+import { memo, useEffect } from 'react';
+import { useSelector } from 'react-redux';
+import { EnvironmentOutlined, LoadingOutlined } from '@ant-design/icons';
+import { useLocation } from '@/hooks/useLocation';
 
 export default memo(() => {
-	const { welcomeText, authorShow } = useSelector<
-		{
-			welcome: TWelcomeType
-		},
-		TWelcomeType
-	>(state => state.welcome)
-	const location = useLocation()
-	useEffect(() => {
-		if (location?.city) {
-			localStorage.setItem('snowIndex-location', JSON.stringify(location))
-		}
-	}, [location])
-	return (
-		<div className=" text-white z-10 relative" id="welcomeNode">
-			<div className="flex justify-between items-center">
-				<p>{welcomeText}</p>
-				<p className="flex justify-center items-center">
-					{location?.city ? (
-						<EnvironmentOutlined className="mr-1" />
-					) : (
-						<LoadingOutlined className="mr-1" />
-					)}
+  const { welcomeText, authorShow } = useSelector<
+    {
+      welcome: TWelcomeType;
+    },
+    TWelcomeType
+  >((state) => state.welcome);
+  const location = useLocation();
+  useEffect(() => {
+    if (location?.city) {
+      localStorage.setItem('snowIndex-location', JSON.stringify(location));
+    }
+  }, [location]);
+  return (
+    <div className=' text-white z-10 relative' id='welcomeNode'>
+      <div className='flex justify-between items-center'>
+        <p>{welcomeText}</p>
+        <p className='flex justify-center items-center'>
+          {location?.city ? (
+            <EnvironmentOutlined className='mr-1' />
+          ) : (
+            <LoadingOutlined className='mr-1' />
+          )}
 
-					{location?.city || '定位中'}
-				</p>
-			</div>
-			{authorShow && (
-				<p>
-					Author{' '}
-					<a
-						className=" text-blue-500 ml-1"
-						href="https://github.com/Jimmylxue"
-					>
-						Jimmyxuexue
-					</a>
-					, reference from coder_yupi, github:
-					<a
-						href="https://github.com/Jimmylxue/snowIndex"
-						className=" text-blue-500 ml-1"
-					>
-						SnowIndex
-					</a>
-				</p>
-			)}
-		</div>
-	)
-})
+          {location?.city || '定位中'}
+        </p>
+      </div>
+      {authorShow && (
+        <p>
+          Author{' '}
+          <a
+            className=' text-blue-500 ml-1'
+            href='https://github.com/Jimmylxue'>
+            Jimmyxuexue
+          </a>
+          , github{' '}
+          <a
+            href='https://github.com/Jimmylxue/snowIndex'
+            className=' text-blue-500 ml-1'>
+            SnowIndex
+          </a>
+          ,{' '}
+          <span className='text-gray-400'>
+            you can input "authorShow off" to hide it ☀️
+          </span>
+        </p>
+      )}
+    </div>
+  );
+});
