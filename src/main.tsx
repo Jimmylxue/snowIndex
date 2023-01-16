3f049e35fe1dbc071f165cee3a4f5da67572e986
jimmyxuexue <1002661758@qq.com>
2023-01-16 22:12:57 +0800

    :bug: 修改为hashRouter

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from '@/stores/store';
 import { SocketProvider } from './hooks/useSocket';
