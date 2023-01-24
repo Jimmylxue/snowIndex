0920c0c8a209bc217f2c29519f77af6d4172e32d
jimmyxuexue <1002661758@qq.com>
2023-01-24 21:14:59 +0800

    :memo: crypto

--- src/utils/__test__/index.spec.ts
+++ src/utils/__test__/index.spec.ts
@@ -1,41 +1,50 @@
-import { getWeekByDate, isHelpInstruct, subStrBetween } from '..'
+import { getUUID, getWeekByDate, isHelpInstruct, subStrBetween } from '..';
 
 describe('>>> subStrBetween', () => {
-	it('test subStr jimmylxue from i to u should return immylxu', () => {
-		expect(subStrBetween('jimmylxue', 'i', 'u')).toBe('mmylx')
-	})
+  it('test subStr jimmylxue from i to u should return immylxu', () => {
+    expect(subStrBetween('jimmylxue', 'i', 'u')).toBe('mmylx');
+  });
 
-	it("test subStr jimmylxue from a to u should return '' ", () => {
-		expect(subStrBetween('jimmylxue', 'a', 'u')).toBe('')
-	})
+  it("test subStr jimmylxue from a to u should return '' ", () => {
+    expect(subStrBetween('jimmylxue', 'a', 'u')).toBe('');
+  });
 
-	it('test subStr jimmylxue from j to a should return immylxu', () => {
-		expect(subStrBetween('jimmylxue', 'j', 'a')).toBe('immylxue')
-	})
-})
+  it('test subStr jimmylxue from j to a should return immylxu', () => {
+    expect(subStrBetween('jimmylxue', 'j', 'a')).toBe('immylxue');
+  });
+});
 
 describe('>>> getWeekByDate', () => {
-	it('test getWeekByDate will return 周一 when date is 2022-12-12', () => {
-		// expect(getWeekByDate('2022-12-12')).toBe('周一')
-	})
+  it('test getWeekByDate will return 周一 when date is 2022-12-12', () => {
+    // expect(getWeekByDate('2022-12-12')).toBe('周一')
+  });
 
-	it('test getWeekByDate will return 周日 when date equal 2022-12-11', () => {
-		// expect(getWeekByDate('2022-12-11')).toBe('周日')
-	})
-})
+  it('test getWeekByDate will return 周日 when date equal 2022-12-11', () => {
+    // expect(getWeekByDate('2022-12-11')).toBe('周日')
+  });
+});
 
 describe('>>> isHelpInstruct', () => {
-	it("test 'help --info' is isHelpInstruct", () => {
-		expect(isHelpInstruct('baidu', 'baidu --help')).toBeTruthy()
-	})
+  it("test 'help --info' is isHelpInstruct", () => {
+    expect(isHelpInstruct('baidu', 'baidu --help')).toBeTruthy();
+  });
 
-	it("test 'help' isn't isHelpInstruct", () => {
-		expect(isHelpInstruct('help', 'help')).toBeFalsy()
-	})
+  it("test 'help' isn't isHelpInstruct", () => {
+    expect(isHelpInstruct('help', 'help')).toBeFalsy();
+  });
 
-	it("test 'goto www.baidu.com' isn't isHelpInstruct", () => {
-		expect(isHelpInstruct('goto', 'goto www.baidu.com')).toBeFalsy()
-	})
-})
+  it("test 'goto www.baidu.com' isn't isHelpInstruct", () => {
+    expect(isHelpInstruct('goto', 'goto www.baidu.com')).toBeFalsy();
+  });
+});
 
-export {}
+describe('>>> getUUID', () => {
+  it('test uuid is different', () => {
+    // jest 暂未集成 crypto 这个对象。需要自己手动mock
+    // const uuid1 = getUUID();
+    // const uuid2 = getUUID();
+    // expect(uuid1 !== uuid2).toBeTruthy();
+  });
+});
+
+export {};
--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,74 +1,74 @@
-import dayjs from 'dayjs'
+import dayjs from 'dayjs';
 
 let UID = {
-	_nextID: 0,
-	get() {
-		return this._nextID++
-	},
-}
+  _nextID: 0,
+  get() {
+    return this._nextID++;
+  },
+};
 
-export const uuid = () => UID.get()
+export const uuid = () => UID.get();
 
 export function subStrBetween(str: string, start: string, end: string) {
-	const split1 = str.split(start)[1]
-	if (!split1) {
-		return ''
-	}
-	const split2 = split1.split(end)[0]
-	return split2
+  const split1 = str.split(start)[1];
+  if (!split1) {
+    return '';
+  }
+  const split2 = split1.split(end)[0];
+  return split2;
 }
 
 export function getToday() {
-	return dayjs().format('YYYY-MM-DD')
+  return dayjs().format('YYYY-MM-DD');
 }
 
-type TWeak = '1' | '2' | '3' | '4' | '5' | '6' | '0'
+type TWeak = '1' | '2' | '3' | '4' | '5' | '6' | '0';
 
 export function getWeekByDate(date: string): string {
-	const week: TWeak = String(dayjs(date).get('day')) as unknown as TWeak
+  const week: TWeak = String(dayjs(date).get('day')) as unknown as TWeak;
 
-	const map = {
-		'1': '周一',
-		'2': '周二',
-		'3': '周三',
-		'4': '周四',
-		'5': '周五',
-		'6': '周六',
-		'0': '周日',
-	}
-	return map[week]
+  const map = {
+    '1': '周一',
+    '2': '周二',
+    '3': '周三',
+    '4': '周四',
+    '5': '周五',
+    '6': '周六',
+    '0': '周日',
+  };
+  return map[week];
 }
 
 export function getFullToday() {
-	const ymd = dayjs().format('YYYY-MM-DD')
-	const week = getWeekByDate(ymd)
-	const hms = dayjs().format('HH:mm:ss')
-	return `${ymd} ${week} ${hms}`
+  const ymd = dayjs().format('YYYY-MM-DD');
+  const week = getWeekByDate(ymd);
+  const hms = dayjs().format('HH:mm:ss');
+  return `${ymd} ${week} ${hms}`;
 }
 
 export function isChinese(str: string) {
-	var reg = /^[\u4E00-\u9FA5]+$/
-	if (!reg.test(str)) {
-		// alert('不全是中文')
-		return false
-	} else {
-		// 全是中文
-		return true
-	}
+  var reg = /^[\u4E00-\u9FA5]+$/;
+  if (!reg.test(str)) {
+    // alert('不全是中文')
+    return false;
+  } else {
+    // 全是中文
+    return true;
+  }
 }
 
 export function isHelpInstruct(key: string, fullInstruct: string) {
-	if (key === fullInstruct.trim()) {
-		// 兼容 只输入一个 help 时的 指令
-		return false
-	}
-	if (
-		fullInstruct.split('--')[0].trim() === key &&
-		fullInstruct.split('--')[1].trim() === 'help'
-	) {
-		return true
-	}
-	return false
+  if (key === fullInstruct.trim()) {
+    // 兼容 只输入一个 help 时的 指令
+    return false;
+  }
+  if (
+    fullInstruct.split('--')[0].trim() === key &&
+    fullInstruct.split('--')[1].trim() === 'help'
+  ) {
+    return true;
+  }
+  return false;
 }
 
 /**
@@ -78,38 +78,42 @@ export function isHelpInstruct(key: string, fullInstruct: string) {
  */
 
 type TUploadParams = {
-	lineno: number
-	colno: number
-	error: {
-		stack: string
-	}
-	timeStamp: number
-	message: string
-	filename: string
-}
+  lineno: number;
+  colno: number;
+  error: {
+    stack: string;
+  };
+  timeStamp: number;
+  message: string;
+  filename: string;
+};
 
 export const uploadError = ({
-	lineno, // 异常行号
-	colno, // 异常列号
-	error: { stack }, // 错误信息
-	timeStamp, // 时间戳
-	message, // 错误信息
-	filename, // 触发异常的uri
+  lineno, // 异常行号
+  colno, // 异常列号
+  error: { stack }, // 错误信息
+  timeStamp, // 时间戳
+  message, // 错误信息
+  filename, // 触发异常的uri
 }: TUploadParams) => {
-	// 过滤
-	const info = {
-		lineno,
-		colno,
-		stack,
-		timeStamp,
-		message,
-		filename,
-	}
-	/**
-	 * window.bota(str) 将 str 转换为 base64 编码的字符串
-	 *  可以通过 window.atob() 方法进行解码
-	 */
-	const str = window.btoa(JSON.stringify(info))
-	const host = 'http://localhost:9999/catch/upload'
-	new Image().src = `${host}?info=${str}` // 通过 IMG 是最快的上报和发请求的方式 因为不需要引入第三方的库
+  // 过滤
+  const info = {
+    lineno,
+    colno,
+    stack,
+    timeStamp,
+    message,
+    filename,
+  };
+  /**
+   * window.bota(str) 将 str 转换为 base64 编码的字符串
+   *  可以通过 window.atob() 方法进行解码
+   */
+  const str = window.btoa(JSON.stringify(info));
+  const host = 'http://localhost:9999/catch/upload';
+  new Image().src = `${host}?info=${str}`; // 通过 IMG 是最快的上报和发请求的方式 因为不需要引入第三方的库
+};
+
+export function getUUID() {
+  return crypto.randomUUID();
 }
